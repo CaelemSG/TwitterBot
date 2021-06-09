c12b24fd70ef8896b4bc2b2821bbd1857b858902
@CaelemSG <caelemsg@gmail.com>
2021-06-09 01:55:50 -0700

    Yeah
    I did stuff

--- reader.py
+++ reader.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import xml
 import xml.etree.ElementTree as ET
 import requests
@@ -9,17 +10,25 @@ status = r.status_code
 data = r.text
 XMLRoot = ET.fromstring(data)
 
+RidingDict = dict()
+with open('RidingNames.csv',encoding='utf-8') as NameFile:
+    FileReader = csv.reader(NameFile)
+    for row in FileReader:
+        row0 = row[0]
+        row1 = row[1]
+        RidingDict[row[1]] = row[0]
+    print(RidingDict)
 
 print(status)
+
 for Voter in XMLRoot:
-    Riding = Voter.find('ConstituencyName')
+    Riding = Voter.find('ConstituencyName').text
+    RidingID = RidingDict[Riding]
     Party = Voter.find('CaucusShortName') 
-    print(Riding.text)
+    print(RidingID)
     print(Party.text)
     YesVote = Voter.find('IsVoteYea').text == 'true'
     if YesVote:
         print('Y')
     else:
         print('N')
-
-    
